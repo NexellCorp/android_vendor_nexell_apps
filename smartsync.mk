80a9b7b2fe494dd971dca40843e41840fdbfd3e2
Sungwoo Park <swpark@nexell.co.kr>
2015-12-28 09:36:44 +0900

    [FIX] remove PRODUCT_COPY_FILES of libstagefright_wfd.so

--- smartsync.mk
+++ smartsync.mk
@@ -1,8 +1,6 @@
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 	vendor/nexell/apps/SmartSync/lib/libect_wfd_jni.so:system/lib/libect_wfd_jni.so \
 	vendor/nexell/apps/SmartSync/lib/libstagefright_wfd.so:system/lib/libstagefright_wfd.so
-#	vendor/nexell/apps/SmartSync/lib/libect_wfd.so:system/lib/libect_wfd.so \
-#	vendor/nexell/apps/SmartSync/lib/libect_foundation.so:system/lib/libect_foundation.so \
 	
 PRODUCT_PACKAGES += \
 	SmartSync
