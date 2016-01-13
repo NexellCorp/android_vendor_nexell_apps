0802c9154127c991bdd9ecb83b09ea5290474925
Sungwoo Park <swpark@nexell.co.kr>
2016-01-13 10:50:08 +0900

    [FIX] UpToDate latest release of SmartMiraSync(1.6.4), SmartAirSync(1.0.2)

--- SmartSync/Android.mk
+++ SmartSync/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SmartSync
-LOCAL_SRC_FILES := app/SmartSync/SmartMiraSync_v1.6.2.apk
+LOCAL_SRC_FILES := app/SmartSync/SmartMiraSync_v1.6.4.apk
 LOCAL_MODULE_PATH := $(TARGET_OUT_APPS)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_OWNER := ELCOMTECH
@@ -13,7 +13,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := SmartAirSync
-LOCAL_SRC_FILES := app/SmartSync/SmartAirSync_v1.0.1.apk
+LOCAL_SRC_FILES := app/SmartSync/SmartAirSync_v1.0.2.apk
 LOCAL_MODULE_PATH := $(TARGET_OUT_APPS)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_OWNER := ELCOMTECH
