0deba4859280bd76e47d8779a419fb4403181ff5
Sungwoo Park <swpark@nexell.co.kr>
2016-02-23 11:56:32 +0900

    remove smartsync, smartairsync package
    After 20160223, smartsync, smartairsync apps are removed from
    default Android Release Packages.

--- smartsync.mk
+++ smartsync.mk
@@ -2,6 +2,6 @@
 	vendor/nexell/apps/SmartSync/lib/libect_wfd_jni.so:system/lib/libect_wfd_jni.so \
 	vendor/nexell/apps/SmartSync/lib/libstagefright_wfd.so:system/lib/libstagefright_wfd.so
 	
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
 	SmartSync \
 	SmartAirSync
