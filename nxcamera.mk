03c7ec05cadd6667a01015b00e8a17e4952b54a8
Sungwon Jo <doriya@nexell.co.kr>
2016-03-08 15:02:43 +0900

    [Android|Application] * Remove Default Application. ( NxCamera )

--- nxcamera.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_COPY_FILES	+= \
-	vendor/nexell/apps/NxCamera/lib/arm/libnx_rgbrender.so:system/app/NxCamera/lib/arm/libnx_rgbrender.so	\
-	vendor/nexell/apps/NxCamera/lib/arm/libnxrgbrender.so:system/app/NxCamera/lib/arm/libnxrgbrender.so
-
-PRODUCT_PACKAGES += \
-	NxCamera
