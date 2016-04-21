9debeb967cb033a6e666d3989329a28b0baa9717
appsw4 <hokahie@naver.com>
2016-04-21 17:07:33 +0900

    [ADD] NxGammaTest apk

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH          :=$(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := NxGammaTest
+LOCAL_SRC_FILES     := app/NxGammaTest/NxGammaTest.apk
+LOCAL_MODULE_PATH   := $(TARGET_OUT_APPS)
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_MODULE_OWNER  := nexell
+LOCAL_MODULE_TAGS   := optional
+LOCAL_CERTIFICATE   := PRESIGNED
+LOCAL_MODULE_SUFFIX   := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+
+include $(BUILD_PREBUILT)
+
