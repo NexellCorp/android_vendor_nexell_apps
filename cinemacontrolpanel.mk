b2784baebf4ed0c7b80498307d61e1230bc35d4b
Sungwon Jo <doriya@nexell.co.kr>
2016-10-28 12:05:54 +0900

    vd_cinema: apk: add CinemaControlPanel for test.
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>

---
+++ cinemacontrolpanel.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES	+= \
+	vendor/nexell/apps/CinemaControlPanel/lib/arm/libnxcinemactrl.so:system/app/CinemaControlPanel/lib/arm/libnxcinemactrl.so	\
+	vendor/nexell/apps/CinemaControlPanel/lib/arm/libnxcinema.so:system/app/CinemaControlPanel/lib/arm/libnxcinema.so
+
+PRODUCT_PACKAGES += \
+	CinemaControlPanel
